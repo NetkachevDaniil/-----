71cb553c915bf3089aa680245b3c616dc3d2c1b7
Daniil Netkachev <daniilnetkacev742@gmail.com>
2025-10-23 21:48:42 +0300

    Update dishes.js

--- js/dishes.js
+++ js/dishes.js
@@ -13,7 +13,7 @@ const dishes = [
     {
         keyword: 'pumpkin-soup',
         name: 'Тыквенный суп-пюре с гренками',
-        price: 330,
+        price: 310,
         category: 'soup',
         count: '320 г',
         image: 'https://tsx.x5static.net/i/800x800-fit/xdelivery/files/10/82/dc3b5ca033d6b00cf13399200cd4.jpg?text=Тыквенный+суп',
@@ -32,28 +32,28 @@ const dishes = [
     {
         keyword: 'mushroom-soup',
         name: 'Грибной суп-пюре',
-        price: 185,
+        price: 320,
         category: 'soup',
         count: '330 г',
-        image: 'https://static.tildacdn.com/tild6139-3265-4436-a439-386136346130/b614e9cc1ffd741cc7f3.png?text=Грибной+суп',
+        image: 'https://tsx.x5static.net/i/800x800-fit/xdelivery/files/e0/4d/c128896e0eea5c177dffc609384b.jpg?text=Грибной+суп',
         kind: 'veg'
     },
     {
         keyword: 'chicken-soup',
         name: 'Куриный суп',
-        price: 330,
+        price: 340,
         category: 'soup',
         count: '350 г',
-        image: 'https://via.placeholder.com/300x200?text=Куриный+суп',
+        image: 'https://static.vecteezy.com/system/resources/previews/036/498/048/non_2x/ai-generated-a-hearty-bowl-of-chicken-and-vegetable-noodle-soup-on-transparent-background-free-png.png?text=Куриный+суп',
         kind: 'meat'
     },
     {
         keyword: 'fish-soup',
         name: 'Уха по-фински',
-        price: 270,
+        price: 320,
         category: 'soup',
         count: '330 г',
-        image: 'https://via.placeholder.com/300x200?text=Уха',
+        image: 'https://static.tildacdn.com/tild6439-3335-4133-b631-386363383664/___.png?text=Уха',
         kind: 'fish'
     },
 
@@ -63,7 +63,7 @@ const dishes = [
         name: 'Стейк из лосося с овощами гриль',
         price: 690,
         category: 'main',
-        count: '320 г',
+        count: '340 г',
         image: 'https://img.freepik.com/premium-photo/plate-grilled-trout-steak-asparagus-with-ingredients-isolated-white-background-top-view_106885-3535.jpg?text=Стейк+лосося',
         kind: 'fish'
     },
@@ -81,7 +81,7 @@ const dishes = [
         name: 'Паста с трюфельным кремом',
         price: 420,
         category: 'main',
-        count: '310 г',
+        count: '330 г',
         image: 'https://avatars.mds.yandex.net/get-mpic/11695357/2a0000018c3967ef07ded4da6b3f5034f676/orig?text=Паста+трюфельная',
         kind: 'veg'
     },
@@ -92,25 +92,25 @@ const dishes = [
         price: 450,
         category: 'main',
         count: '350 г',
-        image: 'https://via.placeholder.com/300x200?text=Бефстроганов',
+        image: 'https://yastatic.net/avatars/get-grocery-goods/2783132/7af72a2e-efd7-425e-9dee-538f87885b89/464x464-origin?text=Бефстроганов',
         kind: 'meat'
     },
     {
         keyword: 'fish-cutlet',
         name: 'Рыбные котлеты с овощами',
-        price: 320,
+        price: 360,
         category: 'main',
-        count: '300 г',
-        image: 'https://via.placeholder.com/300x200?text=Рыбные+котлеты',
+        count: '320 г',
+        image: 'https://gosti-restoran.ru/images/hot_dishes/cuttlets-fish.png?text=Рыбные+котлеты',
         kind: 'fish'
     },
     {
         keyword: 'vegetable-stew',
         name: 'Овощное рагу',
-        price: 280,
+        price: 310,
         category: 'main',
         count: '320 г',
-        image: 'https://via.placeholder.com/300x200?text=Овощное+рагу',
+        image: 'https://avatars.mds.yandex.net/get-eda/3805363/090458f58fc45fbba1aaac72d21d33ef/600x600?text=Овощное+рагу',
         kind: 'veg'
     },
 
@@ -146,19 +146,19 @@ const dishes = [
     {
         keyword: 'orange-juice',
         name: 'Апельсиновый сок',
-        price: 120,
+        price: 130,
         category: 'drink',
-        count: '300 мл',
-        image: 'https://via.placeholder.com/300x200?text=Апельсиновый+сок',
+        count: '320 мл',
+        image: 'https://img.freepik.com/premium-photo/tasty-orange-juice-fresh-fruit-green-leaves-isolated-white_495423-32944.jpg?semt=ais_hybrid?text=Апельсиновый+сок',
         kind: 'cold'
     },
     {
         keyword: 'green-tea',
         name: 'Зеленый чай',
-        price: 100,
+        price: 120,
         category: 'drink',
-        count: '300 мл',
-        image: 'https://via.placeholder.com/300x200?text=Зеленый+чай',
+        count: '280 мл',
+        image: 'https://img.freepik.com/premium-photo/cup-green-tea-with-leaves-white-background_787273-2374.jpg?text=Зеленый+чай',
         kind: 'hot'
     },
     {
@@ -167,7 +167,7 @@ const dishes = [
         price: 150,
         category: 'drink',
         count: '50 мл',
-        image: 'https://via.placeholder.com/300x200?text=Эспрессо',
+        image: 'https://l-emporter-restaurant-snack.fr/wp-content/uploads/2021/04/cafe-decafeine-600x696.png?text=Эспрессо',
         kind: 'hot'
     },
 
@@ -177,8 +177,8 @@ const dishes = [
         name: 'Салат Цезарь с курицей',
         price: 370,
         category: 'salad',
-        count: '220 г',
-        image: 'https://via.placeholder.com/300x200?text=Цезарь',
+        count: '280 г',
+        image: 'https://menubook.ru/assets/plate-sub3-l3VZe9CY.webp?text=Цезарь',
         kind: 'meat'
     },
     {
@@ -187,34 +187,34 @@ const dishes = [
         price: 480,
         category: 'salad',
         count: '250 г',
-        image: 'https://via.placeholder.com/300x200?text=Салат+с+тунцом',
+        image: 'https://ilsushka.ru/pictures/product/big/5703_big.png?text=Салат+с+тунцом',
         kind: 'fish'
     },
     {
         keyword: 'greek-salad',
         name: 'Греческий салат',
-        price: 350,
+        price: 380,
         category: 'salad',
         count: '300 г',
-        image: 'https://via.placeholder.com/300x200?text=Греческий+салат',
+        image: 'https://piccolo-diabolo.ru/wp-content/uploads/2019/05/salat-grecheskij-600x590.png?text=Греческий+салат',
         kind: 'veg'
     },
     {
         keyword: 'caprese',
         name: 'Капрезе с моцареллой',
-        price: 350,
+        price: 310,
         category: 'salad',
         count: '235 г',
-        image: 'https://via.placeholder.com/300x200?text=Капрезе',
+        image: 'https://static.vecteezy.com/system/resources/thumbnails/021/509/077/small_2x/white-plate-with-caprese-salad-isolated-on-a-transparent-background-png.png?text=Капрезе',
         kind: 'veg'
     },
     {
         keyword: 'vegetable-salad',
         name: 'Овощной салат',
-        price: 280,
+        price: 320,
         category: 'salad',
         count: '250 г',
-        image: 'https://via.placeholder.com/300x200?text=Овощной+салат',
+        image: 'https://tenadam.store/wp-content/uploads/2025/04/salad-1.png?text=Овощной+салат',
         kind: 'veg'
     },
     {
@@ -222,8 +222,8 @@ const dishes = [
         name: 'Картофель фри',
         price: 260,
         category: 'salad',
-        count: '235 г',
-        image: 'https://via.placeholder.com/300x200?text=Картофель+фри',
+        count: '220 г',
+        image: 'https://yastatic.net/avatars/get-grocery-goods/2750890/15120968-adb9-4b8f-9a48-15c3b9bac2dd/600x600-orig?text=Картофель+фри',
         kind: 'veg'
     },
 
@@ -234,7 +234,7 @@ const dishes = [
         price: 240,
         category: 'dessert',
         count: '125 г',
-        image: 'https://via.placeholder.com/300x200?text=Чизкейк',
+        image: 'https://nsk.boxfood.su/wp-content/uploads/2020/05/chizkejk-klassicheskij.jpg?text=Чизкейк',
         kind: 'small'
     },
     {
@@ -243,7 +243,7 @@ const dishes = [
         price: 270,
         category: 'dessert',
         count: '150 г',
-        image: 'https://via.placeholder.com/300x200?text=Тирамису',
+        image: 'https://static.vecteezy.com/system/resources/thumbnails/036/497/932/small_2x/ai-generated-tiramisu-dessert-on-a-white-plate-on-transparent-background-png.png?text=Тирамису',
         kind: 'small'
     },
     {
@@ -252,34 +252,34 @@ const dishes = [
         price: 270,
         category: 'dessert',
         count: '140 г',
-        image: 'https://via.placeholder.com/300x200?text=Шоколадный+торт',
+        image: 'https://img.freepik.com/premium-photo/chocolate-cake-white-background_665280-28452.jpg?text=Шоколадный+торт',
         kind: 'small'
     },
     {
         keyword: 'apple-pie',
         name: 'Яблочный пирог',
-        price: 220,
+        price: 240,
         category: 'dessert',
         count: '200 г',
-        image: 'https://via.placeholder.com/300x200?text=Яблочный+пирог',
+        image: 'https://png.pngtree.com/png-clipart/20231120/original/pngtree-high-angle-of-plate-with-thanksgiving-apple-pie-slice-png-image_13660983.png?text=Яблочный+пирог',
         kind: 'medium'
     },
     {
         keyword: 'pancakes',
         name: 'Блинчики с вареньем',
-        price: 180,
+        price: 220,
         category: 'dessert',
         count: '250 г',
-        image: 'https://via.placeholder.com/300x200?text=Блинчики',
+        image: 'https://avatars.mds.yandex.net/get-eda/3502490/c0c772ba59c5d52cf22898fea325289a/400x400nocrop?text=Блинчики',
         kind: 'medium'
     },
     {
         keyword: 'ice-cream-platter',
         name: 'Мороженое ассорти',
-        price: 410,
+        price: 420,
         category: 'dessert',
         count: '350 г',
-        image: 'https://via.placeholder.com/300x200?text=Мороженое',
+        image: 'https://static.vecteezy.com/system/resources/previews/027/735/601/large_2x/four-scoops-of-different-flavour-ice-cream-on-a-plate-free-png.png?text=Мороженое',
         kind: 'large'
     }
 ];
